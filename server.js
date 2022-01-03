2b4cfe35bbb1c3a1bd8d65ed8a228c2526aca9e1
sebli <seblewongelabera@gmail.com>
2022-01-02 21:12:44 -0800

    added read fuction :  finds all store and  find store no using id

--- server.js
+++ server.js
@@ -1,34 +1,30 @@
 const http = require('http');
+const {getAllStores, getStoreById} = require('./controllers/storeController');
+
 const PORT = 500;
-const data = require('./data')
 
-const server = http.createServer((req, res) =>{
-    if (req.url === '/api/v1/stores' && req.method == 'GET') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type','application/json');
-        res.write(JSON.stringify(data[0]));
-        res.end()
-    }
-    else if (req.url === '/api/v1/stores' && req.method == 'POST') {
-        res.statusCode = 200;
-    res.setHeader('Content-Type','application/json');
+const server = http.createServer((req, res) => {
+  if (req.url === '/api/v1/stores' && req.method == 'GET') {
+    getAllStores(req, res);
+  } else if (req.url.match(/\/api\/v1\/stores\/\w+/) && req.method === 'GET') {
+    const idPath = req.url.match(/\/api\/v1\/stores\/\w+/);
+    const id = idPath[0].split('/')[4];
+    getStoreById(req, res, id);
+  } else if (req.url === '/api/v1/stores' && req.method == 'POST') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
     res.write('<h1>POST</h1>');
-    res.end()
-    }
-    else if (req.url === '/api/v1/stores' && req.method == 'DELETE') {
-        res.statusCode = 200;
-    res.setHeader('http','html');
+    res.end();
+  } else if (req.url === '/api/v1/stores' && req.method == 'DELETE') {
+    res.statusCode = 200;
+    res.setHeader('http', 'html');
     res.write('<h1>DELETE</h1>');
-    res.end()
-    }
-    else{
-        res.statusCode = 400;
-        res.setHeader('http','html');
-        res.write('<h1> Bad Connection</h1>');
-        res.end()
-    }
-    
-}
-
-)
-server.listen(PORT,()=> console.log(`server running on port ${PORT}`));
+    res.end();
+  } else {
+    res.statusCode = 400;
+    res.setHeader('http', 'html');
+    res.write('<h1> Bad Connection</h1>');
+    res.end();
+  }
+});
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
