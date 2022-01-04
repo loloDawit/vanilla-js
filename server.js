17f9d199e236c54d77c3cd93fae902cc89843976
dawit.abera <dawit.y.abera@nordstrom.com>
2022-01-04 10:06:47 -0800

    fix lint

--- server.js
+++ server.js
@@ -1,28 +1,25 @@
 const http = require('http');
-const {getAllStores, getStoreById, createStore, updateStore, deleteStore} = require('./controllers/storeController');
+const { getAllStores, getStoreById, createStore, updateStore, deleteStore } = require('./controllers/storeController');
 
 const PORT = 500;
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
-    console.log(req.method);
+  console.log(req.url);
+  console.log(req.method);
 
   if (req.url === '/api/v1/stores' && req.method == 'GET') {
     getAllStores(req, res);
-
   } else if (req.url.match(/\/api\/v1\/stores\/\w+/) && req.method === 'GET') {
     const idPath = req.url.match(/\/api\/v1\/stores\/\w+/);
     const id = idPath[0].split('/')[4];
     getStoreById(req, res, id);
-    
   } else if (req.url === '/api/v1/stores' && req.method == 'POST') {
     createStore(req, res);
-  } else if (req.url.match(/\/api\/v1\/stores\/\w+/) && req.method === 'PUT'){
+  } else if (req.url.match(/\/api\/v1\/stores\/\w+/) && req.method === 'PUT') {
     const idPath = req.url.match(/\/api\/v1\/stores\/\w+/);
     const id = idPath[0].split('/')[4];
     updateStore(req, res, id);
-  }
-  else if (req.url.match(/\/api\/v1\/stores\/\w+/) && req.method === 'DELETE') {
+  } else if (req.url.match(/\/api\/v1\/stores\/\w+/) && req.method === 'DELETE') {
     const idPath = req.url.match(/\/api\/v1\/stores\/\w+/);
     const id = idPath[0].split('/')[4];
     deleteStore(req, res, id);
